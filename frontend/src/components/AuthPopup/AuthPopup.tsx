1fbd1901012ad893932ca68081e4966b6a766300
soyxbshxikh <you@example.com>
2025-03-10 17:22:37 +0530

    Login & Signup page complete

--- frontend/src/components/AuthPopup/AuthPopup.tsx
+++ frontend/src/components/AuthPopup/AuthPopup.tsx
@@ -1,27 +1,149 @@
-import React from 'react'
-import "./AuthPopup.css"
+import React from "react";
+import "./AuthPopup.css";
+import Image from "next/image";
+import logo from "../../assets/logo.png";
+import Input from "@mui/joy/Input";
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
 
 const AuthPopup = () => {
-    const [showSignup, setShowSignup] = React.useState<boolean>(false)
+  const [showSignup, setShowSignup] = React.useState<boolean>(false);
+  const handleLogin = () => { };
+  const handleSignup = () => { };
   return (
-    <div className='popup'>
-      {
-        showSignup ? (
-          <div>
-            <button onClick={()=>{
-              setShowSignup(false)
-            }}>Login</button>
+    <div className="popup">
+      {showSignup ? (
+        <div className="authform">
+          <div className="left">
+            <Image src={logo} alt="Logo" />
           </div>
-        ) : (
-          <div>
-            <button onClick={()=>{
-              setShowSignup(true)
-            }}>Signup</button>
-          </div>
-        )
-      }
-    </div>
-  )
-}
+          <div className="right">
+            <h1>Signup to become a freak</h1>
+            <form action="">
+              <Input
+                color="warning"
+                placeholder="Email"
+                size="lg"
+                variant="outlined"
+              />
+              <Input
+                color="warning"
+                placeholder="Password"
+                size="lg"
+                variant="outlined"
+                type="password"
+              />
+              <div className="form_input_leftright">
+                <Input
+                  color="warning"
+                  size="lg"
+                  variant="outlined"
+                  type="number"
+                  placeholder="
+                  age"
+                />
+                <Input
+                  color="warning"
+                  size="lg"
+                  variant="outlined"
+                  type="number"
+                  placeholder="
+                  weight"
+                />
+              </div>
+              <Select
+                color="warning"
+                placeholder="Gender"
+              >
+                <Option value="male">Male</Option>
+                <Option value="female">female</Option>
+                <Option value="other">Other</Option>
+              </Select>
+              <br />
+              <label htmlFor="">Height</label>
+              <div className="form_input_leftright">
+                <Input
+                  color="warning"
+                  size="lg"
+                  variant="outlined"
+                  type="number"
+                  placeholder="
+                  ft"
+                />
+                <Input
+                  color="warning"
+                  size="lg"
+                  variant="outlined"
+                  type="number"
+                  placeholder="
+                  in"
+                />
+              </div>
 
-export default AuthPopup
+              <button
+                onClick={() => {
+                  handleSignup();
+                }}
+              >
+                Signup
+              </button>
+            </form>
+            <p>
+              Already have an account?{" "}
+              <button
+                onClick={() => {
+                  setShowSignup(false);
+                }}
+              >
+                Login{" "}
+              </button>
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="authform">
+          <div className="left">
+            <Image src={logo} alt="Logo" />
+          </div>
+          <div className="right">
+            <h1>Login to become a freak</h1>
+            <form action="">
+              <Input
+                color="warning"
+                placeholder="Email"
+                size="lg"
+                variant="outlined"
+              />
+              <Input
+                color="warning"
+                placeholder="Password"
+                size="lg"
+                variant="outlined"
+                type="password"
+              />
+              <button
+                onClick={() => {
+                  handleLogin();
+                }}
+              >
+                Login
+              </button>
+            </form>
+            <p>
+              Don't have an account?{" "}
+              <button
+                onClick={() => {
+                  setShowSignup(true);
+                }}
+              >
+                Signup{" "}
+              </button>
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AuthPopup;
